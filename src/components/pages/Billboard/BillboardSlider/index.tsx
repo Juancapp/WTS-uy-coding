033ee088d8be8e6716738d1e7f3fe592d9904bf4
Juancapp <juan.cappuccio@radiumrocket.com>
2024-03-31 19:08:25 -0300

    DV/feature: finish billboard status

--- src/components/pages/Billboard/BillboardSlider/index.tsx
+++ src/components/pages/Billboard/BillboardSlider/index.tsx
@@ -1,18 +1,48 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import Slider from "react-slick";
 import { MovieType } from "../../../../types";
 import "./index.css";
 import { useEffect, useState } from "react";
 import CardsContainer from "../CardsContainer";
 import { useMoviesStore } from "../../../../zustand/store";
+import Card from "../Card";
 
 function BillboardSlider() {
+  const [width, setWidth] = useState(window.innerWidth);
+
   const settings = {
     infinite: true,
-    speed: 500,
+    speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
   };
+
+  const mobileSettings = {
+    ...settings,
+    className: "center",
+    centerPadding: "90px",
+    slidesToShow: 2,
+    swipeToSlide: true,
+  };
+
+  const settingsToComponents = width < 768 ? mobileSettings : settings;
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(width);
+  }, [width]);
 
   const moviesData = useMoviesStore((state) => state.moviesData);
 
@@ -34,15 +64,21 @@ function BillboardSlider() {
 
       setMoviesArrays(tempArrays);
     }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [moviesData]);
+  }, [moviesData?.length]);
 
   return (
-    <Slider {...settings}>
-      {moviesArrays.map((moviesData) => {
-        return <CardsContainer data={moviesData} />;
-      })}
+    <Slider {...settingsToComponents}>
+      {width > 768
+        ? moviesArrays.map((moviesData) => {
+            return <CardsContainer data={moviesData} />;
+          })
+        : moviesData?.map((movie) => {
+            return (
+              <div className="sliderCardContainer">
+                <Card movieData={movie} />
+              </div>
+            );
+          })}
     </Slider>
   );
 }
